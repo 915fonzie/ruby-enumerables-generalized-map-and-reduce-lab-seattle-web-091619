536f8d091ff1e874173a9928a45eb9d1f759c54d
Alfonso Pruneda-Suarez <alfonso.pru@outlook.com>
2019-09-10 21:15:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,14 @@
-# Your Code Here
+def map(source_array)
+  new_array = []
+  source_array.each do |i|
+    new_array.push(yield(i))
+  end
+  new_array
+end
+
+def reduce(source_array, starting_point = 0)
+  source_array.each do |i|
+    
+  end
+  starting_point
+end
